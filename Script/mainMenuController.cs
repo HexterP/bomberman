f7aec6fa1c22159c95aaea2e38bcbb9c3f3f4d4c
hoseinhajepor <hoseinhajepor@yahoo.com>
2019-02-27 18:01:09 +0330

    some chnage

---
+++ Script/mainMenuController.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class mainMenuController : MonoBehaviour
+{
+
+    public GameObject main_menu;
+    public GameObject joystick;
+    public GameObject score_area;
+
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void playgame()
+    {
+        main_menu.SetActive(false);
+        joystick.SetActive(true);
+        score_area.SetActive(true);
+    }
+}
