45525a52c6ce69e182c5699b35c0f85187760ae5
hoseinhajepor <hoseinhajepor@yahoo.com>
2019-02-27 22:24:28 +0330

    add enemy

--- Script/Grenades.cs
+++ Script/Grenades.cs
@@ -6,7 +6,9 @@ public class Grenades : MonoBehaviour
 {
     public float targetTime = 60.0f;
     public GameObject explode;
-
+    public GameObject wave;
+    public float blastRadius = 10.0f;
+    public float explosionForce = 700.0f;
     void Update()
     {
 
@@ -22,6 +24,25 @@ public class Grenades : MonoBehaviour
     void timerEnded()
     {
         Instantiate(explode, transform.position, transform.rotation);
+        //Instantiate(wave, transform.position, transform.rotation);
+
+        Collider[] colliders = Physics.OverlapSphere(transform.position, blastRadius);
+
+        foreach(Collider nearby in colliders)
+        {
+            Rigidbody rb = nearby.GetComponent<Rigidbody>();
+            if(rb != null)
+            {
+                rb.AddExplosionForce(explosionForce,transform.position, blastRadius);
+            }
+
+            health health_actor = nearby.GetComponent<health>();
+            if(health_actor != null)
+            {
+                health_actor.health_ -= 50;
+            }
+            Debug.Log(nearby.gameObject.name);
+        }
         Destroy(gameObject);
     }
 }
---
+++ Script/health.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class health : MonoBehaviour
+{
+
+    public int health_=100;
+
+    public Animator ainm;
+
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (health_ <= 0)
+        {
+            ainm.Play("dead");
+            Destroy(gameObject, 3f);
+        }
+    }
+
+    
+}
