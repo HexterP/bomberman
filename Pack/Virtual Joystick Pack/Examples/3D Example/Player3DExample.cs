0fe91d5570a7700bdd54d194b1c4316c68c70db1
hoseinhajepor <hoseinhajepor@yahoo.com>
2019-02-27 17:47:02 +0330

    second commit

---
+++ Pack/Virtual Joystick Pack/Examples/3D Example/Player3DExample.cs
@@ -0,0 +1,43 @@
+﻿using UnityEngine;
+
+public class Player3DExample : MonoBehaviour {
+
+    public float moveSpeed = 8f;
+    public Joystick joystick;
+    public Transform fire_point;
+    public GameObject Grenades;
+    public Animator anim;
+
+    int idleHash = Animator.StringToHash("idle");
+    int grenadeHash = Animator.StringToHash("grenade");
+    int runHash = Animator.StringToHash("run");
+    bool fire=true;
+
+	void Update () 
+	{
+        Vector3 moveVector = (Vector3.right * joystick.Horizontal + Vector3.forward * joystick.Vertical);
+
+        if (moveVector != Vector3.zero)
+        {
+            transform.rotation = Quaternion.LookRotation(moveVector);
+            transform.Translate(moveVector * moveSpeed * Time.deltaTime, Space.World);
+            anim.Play("run");
+            fire = false;
+        }
+        else
+        {
+            if (fire == false)
+            {
+                Debug.Log("fire");
+                Instantiate(Grenades, fire_point.position, fire_point.rotation);
+                fire = true;
+
+                anim.Play("grenade");
+            }
+            else
+            {
+             //   anim.Play("idle");
+            }
+        }
+	}
+}
---
+++ Script/Grenades.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Grenades : MonoBehaviour
+{
+    public float targetTime = 60.0f;
+    public GameObject explode;
+
+    void Update()
+    {
+
+        targetTime -= Time.deltaTime;
+
+        if (targetTime <= 0.0f)
+        {
+            timerEnded();
+        }
+
+    }
+
+    void timerEnded()
+    {
+        Instantiate(explode, transform.position, transform.rotation);
+        Destroy(gameObject);
+    }
+}
